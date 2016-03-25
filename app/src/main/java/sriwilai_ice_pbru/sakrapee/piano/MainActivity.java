8037e13b62fb9f0985334b3e4d77ab7163c6eaab
PoundENGI <pondnonnoypp@gmail.com>
2016-03-25 17:14:39 +0700

    test

---
+++ app/src/main/java/sriwilai_ice_pbru/sakrapee/piano/MainActivity.java
@@ -0,0 +1,13 @@
+package sriwilai_ice_pbru.sakrapee.piano;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
